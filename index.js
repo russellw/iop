350b08f26a3b074784309fd1037364a99adba117
Russell Wallace <russell.wallace@gmail.com>
2017-04-09 20:59:35 +0100

    ctype and functional map

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 'use strict'
 var util = require('util')
 
+function get(m, key) {
+	while (m) {
+		if (m.key === key)
+			return m.val
+		m = m.outer
+	}
+}
+
+function isalnum(c) {
+	return isalpha(c) || isdigit(c)
+}
+
+function isalpha(c) {
+	return islower(c) || isupper(c)
+}
+
+function isdigit(c) {
+	return '0' <= c && c <= '9'
+}
+
+function islower(c) {
+	return 'a' <= c && c <= 'z'
+}
+
+function isupper(c) {
+	return 'A' <= c && c <= 'Z'
+}
+
 function print(a) {
 	console.log(util.inspect(a, {
 		colors: process.stdout.isTTY,
@@ -8,4 +36,12 @@ function print(a) {
 		maxArrayLength: null,
 		showHidden: false,
 	}))
+}
+
+function put(m, key, val) {
+	return {
+		key,
+		outer: m,
+		val,
+	}
 }
